a9b2758aa5f402f59418281fed794b608c07c2f6
Ryan Sauers <rlsauers08@gmail.com>
2024-11-11 16:47:57 -0600

    init

---
+++ tests/unit/routes/new-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'flashcard/tests/helpers';
+
+module('Unit | Route | new', function (hooks) {
+  setupTest(hooks);
+
+  test('it exists', function (assert) {
+    let route = this.owner.lookup('route:new');
+    assert.ok(route);
+  });
+});
